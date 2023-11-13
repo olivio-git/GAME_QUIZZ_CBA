7d98902b9443d87fcdb1de6657527414e365cb68
limber-git <limber.rodrigo.tj@gmail.com>
2023-11-13 08:54:10 -0400

    Checkbox

--- FRONTEND/src/components/GameStart.jsx
+++ FRONTEND/src/components/GameStart.jsx
@@ -5,14 +5,15 @@ import { fetchGetCategory } from '../utils/fetchBackend';
 import dataQuestions from '../utils/exampleQuestions.json';
 
 const GameStart = () => {
-    const [modalQuestion,setModalQuestion]=useState(false);
-    const [questionGameIn,setQuestinGameIn]=useState(null);
+    const [modalQuestion, setModalQuestion] = useState(false);
+    const [questionGameIn, setQuestinGameIn] = useState(null);
     const { gameContext, categorys, addCategorys } = useContext(DataContext);
-    
+
     const stateRender = (question) => {
         setQuestinGameIn(question)
         setModalQuestion(true);
     }
+
     const renderModalQuestion = () => {
         return (
             <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
@@ -20,26 +21,39 @@ const GameStart = () => {
                     <h1 className="text-2xl font-bold mb-4">
                         {questionGameIn.question}
                     </h1>
-                    <div className='mb-4'>
+                    <div className="mb-4">
                         <h2 className="text-xl font-semibold mb-2">Responses</h2>
                         <div className="pl-4">
-                            {
-                                questionGameIn.answer.map((a, index) => {
-                                    return(
-                                        <div>
-                                            <p key={index} className="mb-2">{a.response}</p>
-                                        </div>
-                                    )
-                                })
-                            }
+                            {questionGameIn.answer.map((a, index) => (
+                                <div key={index} className="mb-2">
+                                    <label className="inline-flex items-center">
+                                        <input
+                                            type="radio"
+                                            name="response"
+                                            value={a.response}
+                                            checked={a.selected} 
+                                            onChange={() => handleResponseSelect(index)}
+                                            className="mr-2"
+                                        />
+                                        {a.response}
+                                    </label>
+                                </div>
+                            ))}
                         </div>
                     </div>
-                    <button className='py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600' onClick={()=>setModalQuestion(false)}>Cerrar</button>
+                    <button
+                        className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600"
+                        onClick={() => setModalQuestion(false)}
+                    >
+                        Cerrar
+                    </button>
                 </div>
             </div>
         );
-    }
-    
+    };
+
+
+
     return (
         <div className='grid grid-cols-3 grid-rows-5 w-full gap-1'>
             <div className='grid grid-cols-3 col-span-3 border bg-white border-none rounded-2xl p-2 shadow shadow-2xl'>
@@ -91,7 +105,7 @@ const GameStart = () => {
                         return (
                             <div key={c.id_category} className='col-span-1 row-span-1'>
                                 <button type="button"
-                                    className="w-full  h-full bg-white rounded-2xl shadow shadow-2xl">
+                                    className="w-full  h-full bg-white rounded-2xl shadow">
                                     <h1 className='text-4xl font-extrabold leading-none text-red-400 md:text-2xl lg:text-2xl '>
                                         {c.name_category}
                                     </h1>
@@ -105,12 +119,12 @@ const GameStart = () => {
                         return (
                             <div key={c.index} className='col-span-1  row-span-5 grid grid-rows-5 gap-2 '>
                                 {
-                                    dataQuestions.map((q,ind) => {
+                                    dataQuestions.map((q, ind) => {
                                         if (q.CategoryIdCategory == categorys[index].id_category) {
                                             return (
-                                                <div key={ind+1} onClick={()=>stateRender(q)} className="flex items-center justify-center
-                                                font-bold text-gray-600 bg-white row-span-1 rounded-2xl shadow shadow-2xl hover:cursor-pointer">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="35" fill='purple' height="35"><path d="M12,19c-.829,0-1.5-.672-1.5-1.5,0-1.938,1.352-3.709,3.909-5.118,1.905-1.05,2.891-3.131,2.51-5.301-.352-2.003-1.997-3.648-4-4-1.445-.254-2.865,.092-4.001,.974-1.115,.867-1.816,2.164-1.922,3.559-.063,.825-.785,1.445-1.609,1.382-.826-.063-1.445-.783-1.382-1.609,.17-2.237,1.29-4.315,3.073-5.7C8.89,.278,11.149-.275,13.437,.126c3.224,.566,5.871,3.213,6.437,6.437,.597,3.399-1.018,6.794-4.017,8.447-1.476,.813-2.357,1.744-2.357,2.49,0,.828-.671,1.5-1.5,1.5Zm-1.5,3.5c0,.828,.672,1.5,1.5,1.5s1.5-.672,1.5-1.5-.672-1.5-1.5-1.5-1.5,.672-1.5,1.5Z"/></svg>
+                                                <div key={ind + 1} onClick={() => stateRender(q)} className="flex items-center justify-center
+                                                font-bold text-gray-600 bg-white row-span-1 rounded-2xl shadow-2xl hover:cursor-pointer">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="35" fill='purple' height="35"><path d="M12,19c-.829,0-1.5-.672-1.5-1.5,0-1.938,1.352-3.709,3.909-5.118,1.905-1.05,2.891-3.131,2.51-5.301-.352-2.003-1.997-3.648-4-4-1.445-.254-2.865,.092-4.001,.974-1.115,.867-1.816,2.164-1.922,3.559-.063,.825-.785,1.445-1.609,1.382-.826-.063-1.445-.783-1.382-1.609,.17-2.237,1.29-4.315,3.073-5.7C8.89,.278,11.149-.275,13.437,.126c3.224,.566,5.871,3.213,6.437,6.437,.597,3.399-1.018,6.794-4.017,8.447-1.476,.813-2.357,1.744-2.357,2.49,0,.828-.671,1.5-1.5,1.5Zm-1.5,3.5c0,.828,.672,1.5,1.5,1.5s1.5-.672,1.5-1.5-.672-1.5-1.5-1.5-1.5,.672-1.5,1.5Z" /></svg>
                                                 </div>
 
                                             )
@@ -122,7 +136,7 @@ const GameStart = () => {
                     })
                 }
                 {
-                    modalQuestion ? renderModalQuestion():null
+                    modalQuestion ? renderModalQuestion() : null
                 }
             </div>
         </div>
