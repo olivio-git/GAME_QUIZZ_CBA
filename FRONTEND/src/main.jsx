f0e13f8c64302c8a33b6b6804683044bfa195f9c
limber-git <limber.rodrigo.tj@gmail.com>
2023-11-27 16:15:47 -0400

    rr

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -11,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <BrowserRouter>
       <DataContextProvider>
+        
         <App />
       </DataContextProvider>
     </BrowserRouter>
