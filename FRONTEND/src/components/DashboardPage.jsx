54602d049e75ce71f2f88e93d2b674840cfbc05b
limber-git <limber.rodrigo.tj@gmail.com>
2023-11-28 17:06:59 -0400

    limber

--- FRONTEND/src/components/DashboardPage.jsx
+++ FRONTEND/src/components/DashboardPage.jsx
@@ -378,7 +378,7 @@ const DashboardPage = () => {
                                 <option value="hard">Hard</option>
                             </select>
                         </div>
-                        <button
+                        {/* <button
                             type="submit"
                             className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
                         >
@@ -390,7 +390,7 @@ const DashboardPage = () => {
                             className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
                         >
                             Cancel
-                        </button>
+                        </button> */}
                     </form>
                 </div>
             </div>
