8f71d6cdd8f618467795e6e04dbd7d32213a0f2b
limber-git <limber.rodrigo.tj@gmail.com>
2023-11-28 00:56:24 -0400

    limber

--- FRONTEND/src/components/Menu.jsx
+++ FRONTEND/src/components/Menu.jsx
@@ -15,7 +15,7 @@ const Menu = () => {
                                 Admin
                                 <span className="absolute inset-0"></span>
                             </Link>
-                            <p className="mt-1 text-gray-600">View game history</p>
+                            <p className="mt-1 text-gray-600">Manage Questions</p>
                         </div>
                     </div>
                     <div className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
