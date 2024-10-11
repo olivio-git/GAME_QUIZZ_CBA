b5edd04d68e51de449042e9fc7ac7dc4d7e8e82c
limber-git <limber.rodrigo.tj@gmail.com>
2024-10-11 11:35:52 -0400

    NewModalquestion, modalSucces, modalError, new design

--- FRONTEND/src/utils/emvironments.js
+++ FRONTEND/src/utils/emvironments.js
@@ -9,7 +9,7 @@ export const KEY_LOCAL_STORAGE_USEDQUESTIONS = "questions";
 export const KEY_LOCAL_STORAGE_TURN = "turnSave";
 
 export const VALUE_ROUNDS_LOCAL = 2;
-export var VALUE_INTERVAL_COUNTER = 25;
+export var VALUE_INTERVAL_COUNTER = 20;
 
 // export const KEY_LOCAL_STORAGE_TURN = "turnSave";
 
